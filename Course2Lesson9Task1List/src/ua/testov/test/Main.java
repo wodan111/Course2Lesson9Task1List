a51113f91ab43ce367889b3e1b20ca251a140593
Vadim <testov@test.ua>
2019-02-26 20:54:47 +0200

    .

--- Course2Lesson9Task1List/src/ua/testov/test/Main.java
+++ Course2Lesson9Task1List/src/ua/testov/test/Main.java
@@ -8,16 +8,17 @@ public class Main {
 	public static void main(String[] args) {
 		list();
 	}
-public static void list() {
-	List<Integer> listOne = new ArrayList<Integer>();
-	for (int i = 0; i < 10; i++) {
-		listOne.add((int) (Math.random() * 50));
+
+	public static void list() {
+		List<Integer> listOne = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			listOne.add((int) (Math.random() * 50));
+		}
+		// System.out.println(listOne);
+		int x = listOne.get(9);
+		listOne.remove(0);
+		listOne.remove(0);
+		listOne.remove(listOne.lastIndexOf(x));
+		System.out.println(listOne);
 	}
-	//System.out.println(listOne);
-	int x = listOne.get(9);
-	listOne.remove(0);
-	listOne.remove(0);
-	listOne.remove(listOne.lastIndexOf(x));
-	System.out.println(listOne);
-}
 }
